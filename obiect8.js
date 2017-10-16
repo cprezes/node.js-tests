69a797db4ee07dbd9d9b4d49fb1bf2c1abc51c5e
cPrezes <cprezes@cprezes>
2017-10-16 23:00:21 +0200

    Continue

--- obiect8.js
+++ obiect8.js
@@ -9,3 +9,10 @@ function greet(name, greeting) {
   console.log(greet('James')); // Welcome James!
   console.log(greet('Richard', 'Howdy')); // Howdy Richard!
   
+  function greet1(name = 'Student', greeting = 'Welcome') {
+    return `${greeting} ${name}!`;
+  }
+  
+  console.log( greet1()); // Welcome Student!
+  console.log(greet1('John')); 
+  console.log(greet1('Red', 'Socks')); 
